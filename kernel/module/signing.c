6a80aa7b22ce9deb77b4dceddbd61588c174ce3a
Michal Suchanek <msuchanek@suse.de>
2021-11-04 12:57:34 +0100

    module: Move duplicate mod_check_sig users code to mod_parse_sig
    Multiple users of mod_check_sig check for the marker, then call
    mod_check_sig, extract signature length, and remove the signature.
    
    Put this code in one place together with mod_check_sig.
    
    This changes the error from ENOENT to ENODATA for ima_read_modsig in the
    case the signature marker is missing.
    
    This also changes the buffer length in ima_read_modsig from size_t to
    unsigned long. This reduces the possible value range on 32bit but the
    length refers to kernel in-memory buffer which cannot be longer than
    ULONG_MAX.
    
    Also change mod_check_sig to unsigned long while at it.
    
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>
    Reviewed-by: Luis Chamberlain <mcgrof@kernel.org>
    ---
    v3: - Philipp Rudo <prudo@redhat.com>: Update the commit with note about
          change of raturn value
        - Preserve the EBADMSG error code while moving code araound
    v4: - remove unused variable ms in module_signing.c
        - note about buffer length
    v5: - also change the functions in module_signature.c to unsigned long

--- kernel/module/signing.c
+++ kernel/module/signing.c
@@ -48,35 +48,16 @@ int verify_appended_signature(const void *data, unsigned long *len,
 			      struct key *trusted_keys,
 			      enum key_being_used_for purpose)
 {
-	const unsigned long markerlen = sizeof(MODULE_SIG_STRING) - 1;
-	const struct module_signature *ms;
-	unsigned long sig_len, modlen = *len;
+	unsigned long sig_len;
 	int ret;
 
-	pr_devel("==>%s %s(,%lu)\n", __func__, key_being_used_for[purpose], modlen);
+	pr_devel("==>%s %s(,%lu)\n", __func__, key_being_used_for[purpose], *len);
 
-	if (markerlen > modlen)
-		return -ENODATA;
-
-	if (memcmp(data + modlen - markerlen, MODULE_SIG_STRING,
-		   markerlen))
-		return -ENODATA;
-	modlen -= markerlen;
-
-	if (modlen <= sizeof(*ms))
-		return -EBADMSG;
-
-	ms = data + modlen - sizeof(*ms);
-
-	ret = mod_check_sig(ms, modlen, key_being_used_for[purpose]);
+	ret = mod_parse_sig(data, len, &sig_len, key_being_used_for[purpose]);
 	if (ret)
 		return ret;
 
-	sig_len = be32_to_cpu(ms->sig_len);
-	modlen -= sig_len + sizeof(*ms);
-	*len = modlen;
-
-	return verify_pkcs7_signature(data, modlen, data + modlen, sig_len,
+	return verify_pkcs7_signature(data, *len, data + *len, sig_len,
 				      trusted_keys,
 				      purpose,
 				      NULL, NULL);
